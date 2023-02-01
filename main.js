75fb64f84be2e8a95648ea804cfa7ab1816a6122
Sebastian Pedersen Robles <sebastian.pedersen.2000@gmail.com>
2023-02-01 22:43:10 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,55 @@
+fetch("actors.json")
+    .then(response => {
+        if (!response.ok) {
+            throw Error(response.statusText);
+        }
+        return response.json()
+    })
+
+    .then((data) => {
+        console.log(data)
+        handleData(data);
+
+    })
+
+    .catch((e) => {
+        console.error("an error occured", e.message)
+    });
+
+function handleData(actors) {
+    actors.forEach((actor) => {
+        const myTemplate = document.querySelector("template").content;
+        const clone = myTemplate.cloneNode(true);
+        clone.querySelector("#name").textContent = actor.fullname;
+        clone.querySelector(".movie").textContent = actor.movie;
+
+        const mainEl = document.querySelector("main");
+
+        mainEl.appendChild(clone);
+
+
+        console.log(actor);
+    });
+
+const element = document.getElementById("myBtn");
+element.addEventListener("click", function () {
+    removeHidden()
+});
+    // document.querySelector(".movie").addEventlistener("click",() => removeHidden());
+};
+
+
+
+
+function removeHidden() {
+    document.querySelector(".movie").classList.remove("black");
+    console.log("hi");
+};
+
+
+
+
+// .then(function(actors){
+//     document.querySelector("myTemplate").content;
+
+// })
